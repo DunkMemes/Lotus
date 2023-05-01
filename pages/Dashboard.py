96340783c007fcfee95ff5e0baf31f1ef26f764d
Marco <wi21061@lehre.dhbw-stuttgart.de>
2023-05-01 22:30:11 +0200

    Tabelle für Portfolioübersicht

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -49,9 +49,76 @@ def page_dasboard():
         line = ple.line(pt)
         line
         max = False
-    st.subheader("Your postitions:")
-    positions = lot.get_all_positions()
-    st.write(positions)
 
+    st.subheader("Your positions:")
+    positions = lot.get_all_positions()
+
+    positionsTable = pd.DataFrame(columns=["Symbol", "Amount", "Value", "Performance"])
+
+    for position in positions:
+        symbol = position.symbol
+        amount = int(position.qty)
+        price = float(position.current_price)
+        value = amount * price
+        performance = float(position.unrealized_plpc) * 100
+
+        positionsTable.loc[len(positionsTable)] = [symbol, amount, value, performance]
+
+    positionsTable["Performance"] = positionsTable["Performance"].astype(float)
+
+    format_func = lambda x: f"{x:,.2f}"
+
+    positionsTable = positionsTable.style \
+        .applymap(lambda x: "color: red" if x <= 0 else "color: green", subset=["Performance"]) \
+        .format({
+        "Value": format_func,
+        "Performance": format_func,
+    }) \
+        .hide_index() \
+        .set_properties(**{'font-weight': 'bold'}, subset=['Symbol']) \
+        .set_table_styles([
+        {
+            'selector': 'th',
+            'props': [
+                ('font-size', '14px'),
+                ('text-align', 'left'),
+                ('padding', '6px'),
+                ('background-color', '#F2F2F2'),
+                ('color', '#333333'),
+                ('border-bottom', '1px solid #273346'),
+                ('border-right', '1px solid #273346'),
+                ('border-left', '1px solid #273346'),
+            ]
+        },
+        {
+            'selector': 'td',
+            'props': [
+                ('font-size', '14px'),
+                ('padding', '6px'),
+                ('border-bottom', '1px solid #273346'),
+                ('border-right', '1px solid #273346'),
+                ('border-left', '1px solid #273346'),
+            ]
+        },
+        {
+            'selector': 'tbody tr:hover',
+            'props': [('background-color', '#273346')]
+        },
+    ])
+
+    st.table(positionsTable)
+
+    # CSS to inject contained in a string
+    hide_dataframe_row_index = """
+            <style>
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            </style>
+            """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+
+    # st.write(positions)
 
 page_dasboard()
