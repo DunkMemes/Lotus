63526f4a624640284f5bfb501063b41c9dccc8d0
Andrii Zolotov <wi21166@lehre.dhbw-stuttgart.de>
2023-05-02 23:53:43 +0200

    Making the working starting and ending of bot

--- pages/Moving Average.py
+++ pages/Moving Average.py
@@ -1,4 +1,4 @@
-import threading
+from threading import Thread, Event
 import time
 import streamlit as st
 from api import lotus
@@ -7,20 +7,19 @@ from api import lotus
 def pageMV():
     st.title("Moving Average Strategy")
     st.subheader("Customizing you trading strategy")
-    running = False
     stock = st.text_input("Stock:")
     buy = st.number_input("Buy Quantity:")
     times = st.selectbox("How often would you like to trade?"
-                 , ("Every 15 min", "Every 1 hour", "Every 24 hours"))
+                         , ("Every 15 min", "Every 1 hour", "Every 24 hours"))
     if times == "Every 15 min":
-        tt = 20
+        tt = 5
     elif times == "Every 1 hour":
         tt = 3600
     elif times == "Every 24 hours":
         tt = 86000
 
     dataTime = st.selectbox("Select the timeframe for your data"
-                 , ("Last 24 hours", "Last 7 days", "Last 30 days"))
+                            , ("Last 24 hours", "Last 7 days", "Last 30 days"))
 
     if dataTime == "Last 24 hours":
         sw = 1
@@ -35,34 +34,41 @@ def pageMV():
         lw = 29
         tf = "1D"
 
-    def trade(tt):
-        print(lotus.movingAverageStrategy(self=lotus(), stock=stock, timeframe=tf, short_window=sw, long_window=lw, buy_qty=buy))
-        # st.experimental_rerun()
+    def trade():
+        print(lotus.movingAverageStrategy(self=lotus(), stock=stock, timeframe=tf, short_window=sw, long_window=lw,
+                                          buy_qty=buy))
 
     col1, col2 = st.columns(2)
     counter = 0
     placeholder = st.empty()
-    startButton = st.button("Start")
-    stopButton = st.button("Stop", key=counter)
-    # placeholder.button("Stop", key=counter)
+
+    def start_trading(event: Event, counter: int, tt: int) -> None:
+        if stock == "":
+            st.warning("Please enter a stock first!")
+        if tt is None:
+            st.warning("Please customize your strategy first!")
+        elif stock != "":
+            while True:
+                trade()
+                time.sleep(tt)
+                counter += 1
+                time.sleep(3)
+                if event.is_set():
+                    print("Stopping the process from the start_trading!")
+                    break
+
+    event = Event()
+    thread = Thread(target=start_trading, args=(event, counter, tt,))
+
+    def set_event():
+        event.set()
+
     with col1:
-        if startButton:
-            if stock == "":
-                st.warning("Please enter a stock first!")
-            if tt is None:
-                st.warning("Please customize your strategy first!")
-            elif stock != "":
-                running = 1
-            while running == 1:
-                if stopButton:
-                    running = 0
-                    print("Stopping the process!")
-                    st.experimental_rerun()
-                elif running == 1:
-                    time.sleep(tt)
-                    counter += 1
-                    t1 = threading.Thread(trade(tt))
-                    t1.start()
+        if st.button("Start"):
+            thread.start()
+
+    with col2:
+        st.button("Stop", on_click=set_event, key=counter)
 
 
 pageMV()
